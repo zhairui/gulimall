772504384088717a374e3bd325c2d792c8669332
cosmoswong <18668062061@163.com>
2020-04-23 00:47:54 +0800

    initial commit

---
+++ gulimall-order/src/test/java/com/bigdata/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
