8b68a96223dea856013efb9ceccaff7431912bce
cosmoswong <18668062061@163.com>
2020-04-24 10:18:57 +0800

    配置nacos注册中心，将所有的module注册到nacos上

--- gulimall-member/src/main/java/com/bigdata/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/bigdata/gulimall/member/GulimallMemberApplication.java
@@ -2,8 +2,10 @@ package com.bigdata.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
