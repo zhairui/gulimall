a01b65faa31d1156b5d7650c3bb64fc6b6316da9
cosmoswong <18668062061@163.com>
2020-05-05 17:08:59 +0800

    优化查询 问题：原查询功能，不支持在catId=0时的查询

--- gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,7 +1,10 @@
 package com.bigdata.gulimall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +27,34 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long cateLogId) {
+
+            String queryKey = (String) params.get("key");
+
+            QueryWrapper<AttrGroupEntity> queryWrapper = new QueryWrapper<AttrGroupEntity>();
+            //catelog_id == 0时，按照attr_group_id和attr_group_name进行模糊查询，否则要带上catelog_id
+            if(cateLogId != 0){
+                queryWrapper.eq("catelog_id", cateLogId);
+            }
+            //select * from pms_attr_group WHERE catelog_id = 1 AND (attr_group_id =key or attr_group_name LIKE '%key%');
+            if (StringUtils.isNotEmpty(queryKey)) {
+
+                queryWrapper.and((param) -> {
+                    param.eq("attr_group_id", queryKey)
+                            .or()
+                            .like("attr_group_name", queryKey);
+                });
+            }
+
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    queryWrapper
+            );
+
+            return new PageUtils(page);
     }
 
 }
