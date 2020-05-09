f8a9a26a3e94bfbe989f1d1ef733eac7798f1435
cosmoswong <18668062061@163.com>
2020-05-09 12:09:51 +0800

    功能描述：  1. 商品管理-SPU和SKU的检索  2. API：https://easydoc.xyz/doc/75716633/ZUqEdvA4/9LISLvy7     API: https://easydoc.xyz/doc/75716633/ZUqEdvA4/ucirLq1D

--- gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bigdata.gulimall.product.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -31,6 +33,55 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key=(String)params.get("key");
+        if(StringUtils.isNotEmpty(key)){
+            queryWrapper.and(item ->{
+                item.eq("sku_id",key).or().like("sku_name",key);
+            });
+        }
+
+        String catelogId=(String)params.get("catelogId");
+        if(StringUtils.isNotEmpty(catelogId) && ! "0".equalsIgnoreCase(catelogId)){
+            queryWrapper.eq("catalog_id",catelogId);
+        }
+
+
+        String brandId=(String)params.get("brandId");
+        if(StringUtils.isNotEmpty(brandId) && ! "0".equalsIgnoreCase(brandId)){
+            queryWrapper.eq("brand_id",brandId);
+        }
+
+
+        String min=(String)params.get("min");
+        if(StringUtils.isNotEmpty(min)){
+            queryWrapper.ge("price",min);
+        }
+
+        String max=(String)params.get("max");
+        if(StringUtils.isNotEmpty(max)){
+
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if( bigDecimal.compareTo(new BigDecimal("0")) == 1){
+                    queryWrapper.le("price",max);
+                }
+            } catch (Exception e) {
+
+            }
+        }
+
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),queryWrapper
+        );
+
+        return new PageUtils(page);
     }
 
 }
--- gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/bigdata/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -194,6 +194,39 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
 
     }
 
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SpuInfoEntity> queryWrapper = new QueryWrapper<>();
+
+        String key=(String)params.get("key");
+        if(StringUtils.isNotEmpty(key)){
+            queryWrapper.and(item -> {
+               item.eq("id",key).or().like("spu_name",key);
+            });
+        }
+
+        String status=(String)params.get("status");
+        if(StringUtils.isNotEmpty(status)){
+            queryWrapper.eq("publish_status",status);
+        }
+
+        String brandId=(String)params.get("brandId");
+        if(StringUtils.isNotEmpty(brandId) && (!"0".equalsIgnoreCase(brandId))){
+            queryWrapper.eq("brand_id",brandId);
+        }
+
+        String catelogId=(String) params.get("catelogId");
+        if(StringUtils.isNotEmpty(catelogId) && (!"0".equalsIgnoreCase(catelogId))){
+            queryWrapper.eq("catalog_id",catelogId);
+        }
+
+        IPage<SpuInfoEntity> page = this.page(
+                new Query<SpuInfoEntity>().getPage(params),
+                queryWrapper
+        );
+
+        return new PageUtils(page);
+    }
 
 
     private void saveBaseSpuInfo(SpuInfoEntity spuInfoEntity) {
